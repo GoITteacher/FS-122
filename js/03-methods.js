516d4291c9b75bd2acdbccad3f88836bde56959d
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-09-24 21:45:52 +0300

    update

--- js/03-methods.js
+++ js/03-methods.js
@@ -4,9 +4,90 @@
  * - join
  * - split
  * - slice
+ * - splice
  * - concat
  * - indexOf
  * - push/pop
  */
+//!======================================================
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL'];
 
-const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
+// const message = `Я вивчав: ${courses.join(', ')}`;
+
+// console.log(message);
+
+// const res = courses.join('---');
+
+// console.log(res);
+//!======================================================
+
+// const str = 'HTML, CSS, JavaScript, React, PostgreSQL';
+
+// const arr = str.split('');
+
+// console.log(arr);
+//!======================================================
+// const arr = [1, 2, 4, 6, 7, 3];
+
+// const copy = arr.slice(3, 5);
+
+// console.log(copy);
+
+//!======================================================
+
+// splice
+
+// const arr = [1, 2, 3, 45, 54];
+
+// arr.splice(2, 3, 'Hello', 'World');
+
+// console.log(arr);
+//!======================================================
+
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [5, 6, 7, 8];
+
+// const res = arr1.concat(arr2);
+
+// console.log(res);
+// const res = [].concat(arr1,arr2);
+//!======================================================
+
+// const arr = [1, 3, 6, 632, 2, 4, 6, 7, 4, 3, 3];
+
+// const index = arr.indexOf(4);
+// const index = arr.lastIndexOf(4);
+
+// console.log(index);
+//!======================================================
+
+// const arr = [1, 3, 6, 632, 2, 4, 6, 7, 4, 3, 3];
+
+// console.log(arr.includes(2));
+//!======================================================
+// push pop
+
+const arr = [1, 2, 'Hello world', 3];
+
+// const userNumber = +prompt('Enter number');
+// arr[3] = userNumber;
+// arr[arr.length] = userNumber;
+// console.log(arr);
+
+// arr.push(25);
+// arr.push('The end');
+
+// arr.pop();
+
+// console.log(arr);
+
+//!======================================================
+// arr.unshift(25);
+// arr.shift();
+
+// console.log(arr);
+
+// arr.splice(2, 0, 'Hello world', 'test');
+
+// console.log(arr);
+//!======================================================
--- js/04-iteration.js
+++ js/04-iteration.js
@@ -1,3 +1,4 @@
+var x;
 /**
  * Ітерація по масиву
  *
@@ -6,16 +7,67 @@
  * - Цикл for...of
  * - Різниця у використанні for и for...of
  */
+// const arr = [5000, 1000, 2200, 5500, 1110, 2500];
 
-const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// let sum = 0;
+// sum += arr[0];
+// sum += arr[1];
+// sum += arr[2];
+// sum += arr[3];
+// sum += arr[4];
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
 
+// for (let i = 0; i < arr.length; i += 2) {
+//   arr[i] *= 2;
+// }
+
+// for (let i = 0; i < arr.length; i += 2) {
+//   arr[i] *= 2;
+// }
+
+// console.log(arr);
+
+//!======================================================
+
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+
+// for (const friend of friends) {
+//   console.log(friend);
+//   friend.toUpperCase();
+// }
+
+//!======================================================
+
+// const arr = [1, 2, 4, 5, 2, 3, 35, 56, 2, 4, 6, 45, 3, 42];
+
+// function findIndexes(arr, userValue) {
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === userValue) {
+//       result.push(i);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(findIndexes(arr, 2));
+
+//!======================================================
 /**
  * Напиши скрипт для обчислення площі прямокутника зі сторонами,
  * значення яких зберігаються у змінній values у вигляді рядка.
  * Значення гарантовано розділені пробілом.
  */
 
-const values = '8 11';
+function getArea(values) {
+  const numbers = values.split(' ');
+  const height = +numbers[0];
+  const width = +numbers[1];
+  const area = height * width;
+  return area;
+}
 
 /**
  * Напиши скрипт для перебору масиву fruits.
@@ -25,3 +77,14 @@ const values = '8 11';
  */
 
 const fruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
+
+function task2(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${i + 1}: ${arr[i]}`);
+  }
+}
+
+task2(fruits);
+console.log(x);
+
+var x = 25;
