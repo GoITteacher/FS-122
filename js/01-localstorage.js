037e295ff00c3ffd19b3c3ce102ef9ac3152d045
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-11-05 21:44:20 +0200

    update

--- js/01-localstorage.js
+++ js/01-localstorage.js
@@ -2,8 +2,8 @@
  * LocalStorage
  */
 
-const LS_KEY = "Array of names";
-const names = ["Alice", "Kate", "Emma"];
+const LS_KEY = 'Array of names';
+const names = ['Alice', 'Kate', 'Emma'];
 
 /**
  * Збереження
@@ -34,3 +34,104 @@ const calculator = {
     return this.a + this.b;
   },
 };
+
+//!======================================================
+
+// const value = {
+//   name: 'vasya',
+//   age: 25,
+//   city: 'Dnipro',
+// };
+
+// const zip = JSON.stringify(value);
+
+// console.log(zip);
+
+//!======================================================
+
+// const zip = `{"name":"vasya","age":25,"city":"Dnipro"}`;
+// const data = JSON.parse(zip);
+
+// console.log(data);
+
+//!======================================================
+// const value = { name: 'vasya', key: undefined };
+// console.log(JSON.stringify(value));
+//!======================================================
+
+// JSON.parse('"Hello world"')
+// JSON.parse("'Hello world'")
+// JSON.parse("Hello")
+
+//!======================================================
+// JSON.stringify({}); // -> string
+// JSON.parse(string); // -> data
+
+//!======================================================
+
+// for{}();
+// if(){
+// }esle{
+// }
+
+//!======================================================
+// const age = prompt('Enter your age:');
+// const res = +age + 10.123343;
+// console.log(res.toFixed(2));
+
+//!======================================================
+// try {
+//   const itemsJSON = prompt('Enter json items:');
+//   const arr = JSON.parse(itemsJSON);
+//   const copy = arr.map(el => el * 2);
+//   console.log(copy);
+// } catch (err) {
+//   console.log('ERROR', err);
+// }
+//!======================================================
+
+// const storage = {
+//   key1: 'Hello world',
+
+//   setItem(key, value) {
+//     this[key] = value;
+//   },
+//   getItem(key) {
+//     return this[key];
+//   },
+// };
+
+// storage.setItem('key1', 'Hello world');
+//!======================================================
+
+// localStorage.setItem('test', 'Hello world');
+// const value = localStorage.getItem('test', 'Hello world');
+
+// localStorage.removeItem('test');
+
+// const value = localStorage.getItem('test');
+
+//!======================================================
+
+function saveToLS(key, value) {
+  const zip = JSON.stringify(value);
+  const item = localStorage.getItem(key);
+  if (!item) {
+    localStorage.setItem(key, zip);
+  }
+}
+
+function loadFromLS(key) {
+  const zip = localStorage.getItem(key);
+
+  try {
+    const value = JSON.parse(zip);
+    return value;
+  } catch {
+    return zip;
+  }
+}
+
+//!======================================================
+
+//!======================================================
